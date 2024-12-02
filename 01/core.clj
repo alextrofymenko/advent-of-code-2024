3ba6c0f3175c07be2382079b75d1c94e54e57d7a
alextrofymenko <alextrofymenko@users.noreply.github.com>
2024-12-02 06:12:03 +0100

    Day 1 - part 2

--- 01/core.clj
+++ 01/core.clj
@@ -12,3 +12,12 @@
 (defn part-1 []
   (let [[list-1 list-2] (read-lists)]
     (apply + (map #(if (> %1 %2) (- %1 %2) (- %2 %1)) (sort list-1) (sort list-2)))))
+
+(defn part-2 []
+  (let [[list-1 list-2] (read-lists)
+        freq-2 (frequencies list-2)]
+    (reduce
+     #(+ %1 (if-let [f (get freq-2 %2)]
+              (* %2 f)
+              0))
+     0 list-1)))
