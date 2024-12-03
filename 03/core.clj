9ce93a85115c5a9ae859d488d509670152bdea3f
alextrofymenko <alextrofymenko@users.noreply.github.com>
2024-12-03 06:41:18 +0100

    Day 3 - part 2

--- 03/core.clj
+++ 03/core.clj
@@ -1,4 +1,6 @@
-(ns core)
+(ns core
+  (:require
+   [clojure.string :as str]))
 
 (defn valid-instructions [memory]
   (re-seq #"mul\((\d{1,3}),(\d{1,3})\)" memory))
@@ -8,3 +10,11 @@
    (fn [total [_ n1 n2]]
      (+ total (* (parse-long n1) (parse-long n2))))
    0 (valid-instructions (slurp "03/input.txt"))))
+
+(defn part-2 []
+  (->> (str/replace (slurp "03/input.txt") #"\n" "")
+       (re-seq #"(?:^|do\(\)).*?(?:don't\(\)|$)")
+       (mapcat valid-instructions)
+       (reduce
+        (fn [total [_ n1 n2]]
+          (+ total (* (parse-long n1) (parse-long n2)))) 0)))
