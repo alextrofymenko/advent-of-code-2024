2392eeccf78f2aa18fe3e6376e0f6ae7a2ca7200
alextrofymenko <alextrofymenko@users.noreply.github.com>
2024-12-05 07:12:54 +0100

    Day 5 - part 1

---
+++ 05/core.clj
@@ -0,0 +1,42 @@
+(ns core
+  (:require
+   [clojure.string :as str]))
+
+(defn rules-idx [rules]
+  (reduce
+   (fn [rules rule]
+     (let [[page-1 page-2] (str/split rule #"\|")]
+       (-> rules
+           (update page-1 update :before (fnil conj #{}) page-2)
+           (update page-2 update :after (fnil conj #{}) page-1))))
+   {} (str/split-lines rules)))
+
+(defn read-rules-and-updates [input]
+  (let [[rules updates] (str/split input #"\n\n")]
+    {:rules   (rules-idx rules)
+     :updates (map #(str/split % #",") (str/split-lines updates))}))
+
+(defn page-right-order? [rules page next-pages]
+  (every? #(let [page-rules (get rules page)]
+             (and (or (empty? (:before page-rules))
+                      (contains? (:before page-rules) %))
+                  (or (empty? (:after page-rules))
+                      (not (contains? (:after page-rules) %)))))
+          next-pages))
+
+(defn update-right-order? [rules pages]
+  (loop [[page & next-pages] pages]
+    (when (page-right-order? rules page next-pages)
+      (or (empty? next-pages)
+          (recur next-pages)))))
+
+(defn find-middle-page [pages]
+  (nth pages (int (/ (count pages) 2))))
+
+(defn sum-middle-pages [updates]
+  (apply + (map (comp parse-long find-middle-page) updates)))
+
+(defn part-1 []
+  (let [{:keys [rules updates]} (read-rules-and-updates (slurp "05/input.txt"))]
+    (->> (filter #(update-right-order? rules %) updates)
+         (sum-middle-pages))))
