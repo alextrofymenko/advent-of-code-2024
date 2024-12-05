86381e01d0c9f6e89f1864d042f6869c172b37e4
alextrofymenko <alextrofymenko@users.noreply.github.com>
2024-12-05 05:24:04 +0100

    Day 4 - part 2

--- 04/core.clj
+++ 04/core.clj
@@ -33,3 +33,20 @@
                      (+ total (count (filter #(= search-word %) words))))
                    total))
                0 word-grid)))
+
+(defn combine-diagonals [{:keys [up-right down-right down-left up-left]}]
+  {:forward-slash (concat (reverse (drop 1 down-left)) up-right)
+   :backward-slash (concat (reverse (drop 1 down-right)) up-left)})
+
+(defn part-2 []
+  (let [search-word "MAS"
+        search-words #{search-word (apply str (reverse search-word))}
+        word-grid (read-grid-map (slurp "04/input.txt"))
+        offsets (combine-diagonals (directional-offsets (dec (count search-word))))]
+    (reduce-kv (fn [total coord char]
+                 (if (= char \A)
+                   (let [words (find-words word-grid coord offsets)]
+                     (cond-> total
+                       (= 2 (count (filter #(contains? search-words %) words))) (inc)))
+                   total))
+               0 word-grid)))
