8efc3250a140416f356d97c084974614ff892497
alextrofymenko <alextrofymenko@users.noreply.github.com>
2024-12-18 20:35:48 +0100

    Day 17 - part 2

--- 17/core.clj
+++ 17/core.clj
@@ -21,7 +21,7 @@
   (let [operand (get input (inc pointer))
         result  (/ (:A registers) (bit-shift-left 1 (combo-operand registers operand)))]
     (-> program-state
-        (assoc-in [:registers result-register] (int result))
+        (assoc-in [:registers result-register] (long result))
         (update :pointer + 2))))
 
 (defn bxl [{:keys [registers input pointer] :as  program-state}]
@@ -64,11 +64,60 @@
    6 #(xdv % :B)
    7 #(xdv % :C)})
 
-(defn program-loop [input]
-  (loop [{:keys [input pointer output] :as  program-state} (init-program input)]
+(defn program-loop [initial-state]
+  (loop [{:keys [input pointer output] :as  program-state} initial-state]
     (if-let [instruction (instructions (get input pointer))]
       (recur (instruction program-state))
-      (str/join "," output))))
+      output)))
 
 (defn part-1 []
-  (program-loop (slurp "17/input.txt")))
+  (let [state (init-program (slurp "17/input.txt"))]
+    (str/join "," (program-loop state))))
+
+(defn octal-set-bit [octal-v n bit]
+  (concat (take n octal-v) [bit] (drop (inc n) octal-v)))
+
+(defn octal->long [octal-v]
+  (long (read-string (apply str "0" octal-v))))
+
+(defn compatible-bit [{:keys [input] :as initial-state} octal-v n]
+  (when (< n (count input))
+    (let [n' (- (dec (count input)) n)]
+      (for [bit   (range 8)
+            :let  [register-a (octal->long (octal-set-bit octal-v n bit))
+                   output (program-loop (assoc-in initial-state [:registers :A] register-a))]
+            :when (= (get input n') (get output n'))]
+        bit))))
+
+(defn smallest-octal-v [initial-state octal-v n]
+  (first
+   (for [b (compatible-bit initial-state octal-v n)
+         :let [bs (smallest-octal-v initial-state (octal-set-bit octal-v n b) (inc n))]
+         :when (or (= n (dec (count (:input initial-state)))) (seq bs))]
+     (cons b bs))))
+
+(defn part-2
+  "A hint from Reddit set me on the right path, but I hit a wall again with recursion,
+   and the final result is just me throwing things at said wall until something stuck.
+   I was hoping to get a list of numbers back, but I coudln't figure out how to unfold
+   the nested list I got from from smallest-octal-v, and just by adding 'first' there
+   got me the answer I was looking for. I though I understood lazy-seq, but I do not.
+
+   The trick to the solution is realising that the program transforms 8-bit numbers in
+   reverse order, i.e. the first bit in Register A in octal form becomes the last bit
+   in the output string. For example, comparing octal input vs actual output:
+
+   o3000000000000000 -> [5 5 5 5 5 5 5 5 5 5 5 5 5 5 1 6]
+   o5000000000000000 -> [5 5 5 5 5 5 5 5 5 5 5 5 5 5 1 0] ;; Last digit correct
+   o7000000000000000 -> [5 5 5 5 5 5 5 5 5 5 5 5 5 5 1 2]
+
+   o5600000000000000 -> [5 5 5 5 5 5 5 5 5 5 5 5 5 5 3 0] ;; Last 2 digits correct
+
+   So the most-significant digit in our octal input number is 5. Then next one is 6,
+   and so on. There can be more than one answer at any one stage, for example after
+   [5 6] either 0 or 1 works, but examining the sequence fully may terminate some seqs
+   sooner. That's what I had to use recursion for and got infinitely confused. Maybe
+   one day I will be able to solve it quicker"
+  []
+  (let [{:keys [input] :as initial-state} (init-program (slurp "17/input.txt"))]
+    (octal->long (smallest-octal-v initial-state (vec (repeat (count input) 0)) 0))))
