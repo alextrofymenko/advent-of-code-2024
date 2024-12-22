ea80996c8933f5ae9255b20e3c22388c6eae10d8
alextrofymenko <alextrofymenko@users.noreply.github.com>
2024-12-22 21:44:56 +0100

    Day 22 - part 2

--- 22/core.clj
+++ 22/core.clj
@@ -18,3 +18,17 @@
        (map parse-long)
        (map #(nth (lazy-secret-number %) 2000))
        (apply +)))
+
+(defn price-change-seqs [secret-numbers]
+  (let [ones-vector (into [] (map #(mod % 10)) secret-numbers)]
+    (->> (partition 2 1 ones-vector)
+         (map #(- (second %) (first %1)))
+         (partition 4 1)
+         (map-indexed (fn [i change-seq] [change-seq (get ones-vector (+ i 4))]))
+         (reduce #(cond-> %1 (not (contains? %1 (first %2))) (assoc (first %2) (second %2))) {}))))
+
+(defn part-2 []
+  (let [buyers-prices-idx (->> (str/split-lines (slurp "22/input.txt"))
+                               (map (comp price-change-seqs #(take 2001 %) lazy-secret-number parse-long)))]
+    (->> (into #{} (mapcat keys) buyers-prices-idx)
+         (reduce (fn [bananas change-seq] (max bananas (apply + (keep #(get % change-seq) buyers-prices-idx)))) 0))))
