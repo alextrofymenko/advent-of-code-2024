6037c210ad98aa8089e00c3dc2a301c3e7a415f3
alextrofymenko <alextrofymenko@users.noreply.github.com>
2024-12-23 20:40:06 +0100

    Day 23 - part 2

--- 23/core.clj
+++ 23/core.clj
@@ -1,5 +1,6 @@
 (ns core
   (:require
+   [clojure.set :as set]
    [clojure.string :as str]))
 
 (defn scan-computers [input]
@@ -31,3 +32,39 @@
        (create-index)
        (find-triplets)
        (count)))
+
+(defn biggest-party-for-linked-pcs [index linked-pcs]
+  (let [all-pcs-in-party (reduce-kv #(conj %1 (conj %3 %2)) #{} (select-keys index linked-pcs))]
+    (reduce
+     (fn [bigest-party pcs]
+       (let [party (apply set/intersection (disj all-pcs-in-party pcs))]
+         (if (> (count party) (count bigest-party))
+           party
+           bigest-party)))
+     #{} all-pcs-in-party)))
+
+(defn biggest-party-per-pc
+  "There are at most 13 PCs in any one group because that's the most any one PC
+   is linked to, as seen in the index. This isn't really relevant, but it gave me
+   the idea that sets we're working with aren't really that large.
+   The idea is that for every PC, we look at the PCs it's linked to, and then look
+   at the PCs that they are linked to. Combined together, we should find a set that
+   contains all the PCs, assuming the largest is actually 13, or if it isn't, then
+   we still get the largest group possible for any one PC."
+  [index]
+  (reduce-kv
+   (fn [biggest-party pc linked-pcs]
+     (assoc biggest-party pc (biggest-party-for-linked-pcs index linked-pcs)))
+   {} index))
+
+(defn the-biggest-party [index]
+  (->> (biggest-party-per-pc index) (sort-by (comp count val)) last last))
+
+(defn get-password [group]
+  (str/join #"," (sort group)))
+
+(defn part-2 []
+  (->> (scan-computers (slurp "23/input.txt"))
+       (create-index)
+       (the-biggest-party)
+       (get-password)))
